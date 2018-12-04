7af0623932dd5bfa8b47529da2fd6e29d6855f8f
Orfac <molodeckij.b.2017@fml31.ru>
2018-12-04 13:48:43 +0300

    updated 2-nd lab(no equal to task)

--- 2lab/2.asm
+++ 2lab/2.asm
@@ -8,6 +8,9 @@ data segment public 'data'
 	value dw 26
 	check db 1
 	output db 12 DUP('$'), '$'
+	WORD_MASK EQU 11111111111111111000000000000000b
+	LOW_MASK EQU 00000000000000000100000000000000b
+	HIGH_MASK EQU 10000000000000000000000000000000b
 data ends	
 
 code segment public 'code'
@@ -18,20 +21,18 @@ main:
 		
 start:	  
 	
-	mov ax, data
-	mov ds, ax
-	mov ax, [value]
-	lea si, output
-	wordToTernaryLoop:
-			mov bx, 3
-			div bx
-			add dx, '0'		; преобразование в символ	
-			mov [si], dx		; dest[i] = bx; i++
-			mov dx, 0
-			inc si
-			mov bx, ax
-			cmp bx, 0
-	jg wordToTernaryLoop	
+	mov eax, 170
+	ror eax, 1
+	mov edx, 0 
+	mov ebx, 2
+	div ebx
+
+	ror eax, 1
+	and eax, WORD_MASK
+	cmp edx, 1
+	jne endd
+	or eax, HIGH_MASK 
+	endd:
 	xor eax, eax
 	mov ax, 4c00h	
 	int	21h		
--- 2lab/build_run.bat
+++ 2lab/build_run.bat
@@ -2,3 +2,4 @@ ml /c 2.asm
 link16 2.obj;
 cd ..
 start C:\"Program Files (x86)"\DOSBox-0.74\DOSBox.exe t.txt
+cd 2lab
