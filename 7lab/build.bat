419b75184ce13c3a89fab088af235dd5e6eba670
Orfac <molodeckij.b.2017@fml31.ru>
2018-12-10 11:22:03 +0300

    initialized labs

---
+++ 7lab/build.bat
@@ -0,0 +1,4 @@
+ml /c /Zd /coff 7.asm
+link /SUBSYSTEM:WINDOWS 7.obj
+if exist 7.obj del 7.obj
+pause
---
+++ 8lab/build.bat
@@ -0,0 +1,4 @@
+ml /c /Zd /coff 8.asm
+Link /SUBSYSTEM:WINDOWS 8.obj
+if exist 8.obj del 8.obj
+pause
